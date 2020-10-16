8fe9089c00e33a8be2cfdde34415a5159494edf4
Nusrat Jahan <njnusrat53@gmail.com>
2020-10-16 17:17:25 +0600

    all  done

--- index.js
+++ index.js
@@ -11,23 +11,26 @@ const fileUpload =require('express-fileupload')
 
 //middleware setup
 const app=express()
+app.use(express.static('service'))
 app.use(fileUpload());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
-//database connect
-const uri = "mongodb+srv://creativeAgency:adgjmptw499@cluster0.oawso.mongodb.net/creative-agency?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
 
 //default setup
-app.all('/',(req, res)=>{
+app.get('/',(req, res)=>{
     res.send({
         status: 200,
         message:'Server is running'
     })
 })
+
+//database connect
+const uri = "mongodb+srv://creativeAgency:adgjmptw499@cluster0.oawso.mongodb.net/creative-agency?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+
 
 //Routes setup
 client.connect(err => {
@@ -35,12 +38,14 @@ client.connect(err => {
     const ordersCollection = client.db("creative-agency").collection("orders");
     const servicesCollection = client.db("creative-agency").collection("services");
     const adminsCollection = client.db("creative-agency").collection("admins");
+    console.log('db connect')
 
     /*************REVIEWS****************/
     //add review
-    app.post('addReviews',(req,res)=>{
+    app.post('/addReviews',(req,res)=>{
         const review=req.body;
-        reviewsCollection.insertOne(review).then((result)=>{
+        reviewsCollection.insertOne(review)
+        .then((result)=>{
             res.send(result);
         })
     })
@@ -60,20 +65,6 @@ client.connect(err => {
     })
 
     /*************SERVICES****************/
-    //get service by ID
-    app.get('/services/:id',(req,res)=>{
-        servicesCollection.find({_id:ObjectId(req.params.id)})
-        .toArray((err,documents)=>{
-            if(err){
-                console.log(err);
-                res.status(500).send({
-                    message: err
-                })
-            } else {
-                res.send(documents[0])
-            }
-        })
-    })
 
     //get all services
     app.get('/services',(req,res)=>{
@@ -92,16 +83,16 @@ client.connect(err => {
     //Add service
     app.post('/addService',(req,res)=>{
         const file=req.files.file;
-        const title=req.body.title;
+        const service=req.body.service;
         const description=req.body.description;
-        const newImg=file.data
+       const newImg=file.data
         const encImg=newImg.toString('base64')
         const image={
             contentType:file.mimetype,
             size:file.size,
             img:Buffer.from(encImg,'base64')
         }
-        servicesCollection.insertOne({image, description, title})
+        servicesCollection.insertOne({image:image, description, service})
         .then(result=>{
             res.send(result.insertedCount>0)
         })
@@ -109,22 +100,15 @@ client.connect(err => {
 
     /*************ORDERS****************/
     //get loggedIn person orders by EMAIL
-    app.get('my-orders/:email',(req,res)=>{
-        ordersCollection.find({ email:req.params.email})
-        .toArray((err,documents)=>{
-            if(err){
-                console.log(err);
-                res.status(500).send({
-                    message: err
-                })
-            } else {
-                res.send(documents)
-            }
-        })
+    app.post('/my-orders' , (req, res) => {
+        ordersCollection.find({email: req.body.email})
+            .toArray((err, documents) =>{
+                res.send(documents);
+            })
     })
 
     //get all orders
-    app.get('/order',(req,res)=>{
+    app.get('/orders',(req,res)=>{
         ordersCollection.find().toArray((err,documents)=>{
             if(err){
                 console.log(err);
@@ -146,28 +130,20 @@ client.connect(err => {
     })
 
     //update order
-    app.put('/update',(req,res)=>{
-        const id=req.body.id;
-        ordersCollection.updateOne({_id:ObjectId(id)},{$set:{decision:req.body.decision}},(req, res)=>{
-            if(err){
-                console.log(err);
-                res.status(500).send({
-                    message: err
-                })
-            } else {
-                res.send(documents)
-            }
+    app.patch('/update', (req,res)=>{
+        ordersCollection.updateOne({_id : ObjectId(req.body.id)},{$set: { decision: req.body.decision}})
+        .then(result =>{
+            res.send(result.modifiedCount > 0)
         })
     })
 
     /*************ADMINS****************/
     //add admin
     app.post('/addAdmin',(req,res)=>{
-        adminsCollection.insertOne({admin:req.body.admin})
+        adminsCollection.insertOne(req.body)
         .then(result=>{
-            res.send(result.insertedCount>0)
+            res.send(result)
         })
-        .catch(err=>console.log(err))
     })
 
     //get a admin
